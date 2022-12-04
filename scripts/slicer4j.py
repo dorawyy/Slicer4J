e861cca3780474588890557f19eba8add975b38a
Yingying Wang <wyingying@zeus.ece.ubc.ca>
2022-12-04 11:44:52 -0800

    slicer4j script update, stop using reformat string feature from python3.6 as the machine has only python3.5

--- scripts/slicer4j.py
+++ scripts/slicer4j.py
@@ -24,16 +24,18 @@ def main():
         #print("Jar file does not exist!")
         return
     jar_file = os.path.abspath(jar_file)
+    print('jar_file is: '+ jar_file)
 
     out_dir = options["out_dir"]
     if not os.path.isdir(out_dir):
-        #print(f"{out_dir} does not exist, creating it")
+        print("{} does not exist, creating it", out_dir)
         os.makedirs(out_dir)
     out_dir = os.path.abspath(out_dir)
+    print('out_dir is: ' + out_dir)
 
     dependencies = options["dependencies"]
     if dependencies and not os.path.isdir(dependencies):
-        #print(f"Dependencies directory doesn't exist")
+        print("Dependencies directory doesn't exist")
         return
     if dependencies:
         dependencies = os.path.abspath(options["dependencies"])
@@ -66,45 +68,53 @@ def main():
     log_file, slice_graph = dynamic_slice(jar_file=jar_file, out_dir=out_dir, backward_criterion=backward_criterion,
                                           extra_options=extra_options)
 
-    #print(f"Slice source code lines: {out_dir}/slice.log")
-    #print(f"Raw slice: {out_dir}/raw-slice.log")
-    #print(f"Slice graph: {slice_graph}")
-    #print(f"Slice with dependencies: {out_dir}/slice-dependencies.log")
+    print("Slice source code lines: {}/slice.log".format(out_dir))
+    print("Raw slice: {}/raw-slice.log".format(out_dir))
+    print("Slice graph: {}".format(slice_graph))
+    print("Slice with dependencies: {}/slice-dependencies.log".format(out_dir))
 
 
 def instrument(jar_file: str, out_dir: str) -> str:
     instr_file = "instr-debug.log"
-    #print("Instrumenting the JAR", flush=True)
-    instr_cmd = f"java -Xmx64g -cp \"{slicer4j_dir}/Slicer4J/target/slicer4j-jar-with-dependencies.jar:{slicer4j_dir}/Slicer4J/target/lib/*\" ca.ubc.ece.resess.slicer.dynamic.slicer4j.Slicer -m i -j {jar_file} -o {out_dir}/ -sl {out_dir}/static_log.log -lc {logger_jar} > {out_dir}/{instr_file} 2>&1"
+    print("Instrumenting the JAR " + jar_file, flush=True)
+    instr_cmd = "java -Xmx64g -cp \"{}/Slicer4J/target/slicer4j-jar-with-dependencies.jar:{}/Slicer4J/target/lib/*\" ca.ubc.ece.resess.slicer.dynamic.slicer4j.Slicer -m i -j {} -o {}/ -sl {}/static_log.log -lc {} > {}/{} 2>&1".format(slicer4j_dir, slicer4j_dir, jar_file, out_dir, out_dir, logger_jar, out_dir, instr_file)
+    #print ('instr_cmd is: ' + instr_cmd)
     os.system(instr_cmd)
     instrumented_jar = os.path.basename(jar_file).replace(".jar", "_i.jar")
-    return out_dir + os.sep + instrumented_jar
+    out_instrumented_jar = out_dir + os.sep + instrumented_jar
+    print('instrumented jar is: ' + out_instrumented_jar)
+    return out_instrumented_jar
 
 
 def run(instrumented_jar, dependencies, out_dir, test_class, test_method, main_class_args):
     #print("Running the instrumented JAR", flush=True)
     if main_class_args is None:
+        
         # UNCOMMENT THIS IF USING JUNIT 5
-        #cmd = f"java -Xmx64g -cp \"{script_dir}/JUnitTestRunner.jar:{script_dir}/junit-jupiter-engine-5.7.2.jar:{script_dir}/junit5-4.2.9.jar:{script_dir}/junit-jupiter-api-5.5.2.jar:{script_dir}/junit-platform-commons-1.8.0-M1.jar:{script_dir}/junit-platform-engine-1.8.0-M1.jar:{script_dir}/junit-platform-launcher-1.8.0-M1.jar:{script_dir}/opentest4j-1.2.0.jar:{script_dir}/apiguardian-api-1.1.1.jar:{instrumented_jar}:{dependencies}/*\" JUnitTestRunner {test_class}#{test_method} > {out_dir}/trace_full.log"
+        #cmd = "java -Xmx64g -cp \"{}/JUnitTestRunner.jar:{}/junit-jupiter-engine-5.7.2.jar:{}/junit5-4.2.9.jar:{}/junit-jupiter-api-5.5.2.jar:{}/junit-platform-commons-1.8.0-M1.jar:{}/junit-platform-engine-1.8.0-M1.jar:{}/junit-platform-launcher-1.8.0-M1.jar:{}/opentest4j-1.2.0.jar:{}/apiguardian-api-1.1.1.jar:{}:{}/*\" JUnitTestRunner {}#{} > {}/trace_full.log".format(script_dir,script_dir,script_dir,script_dir,script_dir,script_dir,script_dir,script_dir,script_dir,instrumented_jar,dependencies,test_class,test_method,out_dir)
+        
         # UNCOMMENT THIS IF USING JUNIT 4
-        cmd = f"java -Xmx8g -cp \"{script_dir}/SingleJUnitTestRunner.jar:{script_dir}/junit-4.8.2.jar:{instrumented_jar}:{dependencies}/*\" SingleJUnitTestRunner {test_class}#{test_method} > {out_dir}/trace_full.log"
+        cmd = "java -Xmx64g -cp \"{}/SingleJUnitTestRunner.jar:{}/junit-4.8.2.jar:{}:{}/*\" SingleJUnitTestRunner {}#{} > {}/trace_full.log".format(script_dir,script_dir,instrumented_jar,dependencies,test_class,test_method,out_dir)
+        
+        print('command to run test: ' + cmd)
     else:
         if main_class_args.startswith("\"") and main_class_args.endswith("\""):
             main_class_args = main_class_args[1:-1]
-        cmd = f"java -Xmx64g -cp \"{instrumented_jar}:{dependencies}/*\" {main_class_args} > {out_dir}/trace_full.log"
+        cmd = "java -Xmx64g -cp \"{}:{}/*\" {} > {}/trace_full.log".format(instrumented_jar, dependencies, main_class_args, out_dir)
+    
     #print(f"Running instrumented JAR", flush=True)
     #print(f"------------------------------------")
     os.system(cmd)
     #print(f"------------------------------------")
-    os.system(f"cat {out_dir}/trace_full.log | grep \"SLICING\" > {out_dir}/trace.log")
+    os.system("cat {}/trace_full.log | grep \"SLICING\" > {}/trace.log".format(out_dir,out_dir))
     trace = list()
-    with open(f"{out_dir}/trace.log", 'r') as f:
+    with open("{}/trace.log".format(out_dir), 'r') as f:
         for line in f:
             if "FIELD" in line:
                 del trace[-1]
             trace.append(line.rstrip())
 
-    with open(f"{out_dir}/trace.log", 'w') as f:
+    with open("{}/trace.log".format(out_dir), 'w') as f:
         for t in trace:
             f.write(t + "\n")
 
@@ -116,14 +126,14 @@ def dynamic_slice(jar_file=None, out_dir=None, backward_criterion=None, variable
         #print(f"Slicing from line {backward_criterion} with variables {variables}", flush=True)
     #else:
         #print(f"Slicing from line {backward_criterion}", flush=True)
-    graph_cmd = f"java -Xmx64g -cp \"{slicer4j_dir}/Slicer4J/target/slicer4j-jar-with-dependencies.jar:{slicer4j_dir}/Slicer4J/target/lib/*\" ca.ubc.ece.resess.slicer.dynamic.slicer4j.Slicer -m g -j {jar_file} -t {out_dir}/trace.log -o {out_dir}/ -sl {out_dir}/static_log.log -sd {slicer4j_dir}/models/summariesManual -tw {slicer4j_dir}/models/EasyTaintWrapperSource.txt > {out_dir}/{graph_file} 2>&1"
+    graph_cmd = "java -Xmx64g -cp \"{}/Slicer4J/target/slicer4j-jar-with-dependencies.jar:{}/Slicer4J/target/lib/*\" ca.ubc.ece.resess.slicer.dynamic.slicer4j.Slicer -m g -j {} -t {}/trace.log -o {}/ -sl {}/static_log.log -sd {}/models/summariesManual -tw {}/models/EasyTaintWrapperSource.txt > {}/{} 2>&1".format(slicer4j_dir,slicer4j_dir,jar_file,out_dir,out_dir,out_dir,slicer4j_dir,slicer4j_dir,out_dir,graph_file)
     os.system(graph_cmd)
 
     clazz, lineno = backward_criterion.split(":")
     # clazz = clazz.rsplit(".", 1)[0]
-    check_str = f":LINENO:{lineno}:FILE:{clazz}"
+    check_str = ":LINENO:{}:FILE:{}".format(lineno,clazz)
     slice_line = ""
-    with open(f"{out_dir}/trace.log_icdg.log", 'r') as f:
+    with open("{}/trace.log_icdg.log".format(out_dir), 'r') as f:
         for line in f:
             if check_str in line:
                 if slice_line:
@@ -133,12 +143,12 @@ def dynamic_slice(jar_file=None, out_dir=None, backward_criterion=None, variable
     if variables:
         extra_options += "-sv " + str(variables)
 
-    slice_cmd = f"java -Xmx64g -cp \"{slicer4j_dir}/Slicer4J/target/slicer4j-jar-with-dependencies.jar:{slicer4j_dir}/Slicer4J/target/lib/*\" ca.ubc.ece.resess.slicer.dynamic.slicer4j.Slicer -m s -j {jar_file} -t {out_dir}/trace.log -o {out_dir}/ -sl {out_dir}/static_log.log -sd {slicer4j_dir}/models/summariesManual -tw {slicer4j_dir}/models/EasyTaintWrapperSource.txt -sp {slice_line} {extra_options} > {out_dir}/{slice_file} 2>&1"
+    slice_cmd = "java -Xmx64g -cp \"{}/Slicer4J/target/slicer4j-jar-with-dependencies.jar:{}/Slicer4J/target/lib/*\" ca.ubc.ece.resess.slicer.dynamic.slicer4j.Slicer -m s -j {} -t {}/trace.log -o {}/ -sl {}/static_log.log -sd {}/models/summariesManual -tw {}/models/EasyTaintWrapperSource.txt -sp {} {} > {}/{} 2>&1".format(slicer4j_dir,slicer4j_dir,jar_file,out_dir,out_dir,out_dir,slicer4j_dir,slicer4j_dir,slice_line,extra_options,out_dir,slice_file)
     os.system(slice_cmd)
     arr = [x for x in os.listdir(out_dir) if x.startswith("result_md")]
     for a in arr:
-        os.system(f"rm {out_dir}/{a}")
-    return f"{out_dir}/{slice_file}", f"{out_dir}/slice-graph.pdf"
+        os.system("rm {}/{a}".format(out_dir, a))
+    return "{}/{}".format(out_dir,slice_file), "{}/slice-graph.pdf".format(out_dir)
 
 
 def parse():
@@ -181,4 +191,8 @@ def parse():
 
 
 if __name__ == "__main__":
+    print('script_dir: ' + script_dir )
+    print('slicer4j_dir: ' + slicer4j_dir)
+    print('logger_jar: ' + logger_jar)
+
     main()
